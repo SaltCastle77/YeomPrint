537eef6fafdf5cf56e4eb02e8972c956a338e7c1
lovelove33j1 <lovelove33j1@naver.com>
2021-11-15 11:29:16 +0900

    프린트문작성

--- Example/YeomPrint/ViewController.swift
+++ Example/YeomPrint/ViewController.swift
@@ -7,12 +7,14 @@
 //
 
 import UIKit
+import YeomPrint
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        Yeom.sayHello(with: "성훈")
     }
 
     override func didReceiveMemoryWarning() {
