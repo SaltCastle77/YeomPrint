b64bd0946c01aa1a75fbc09d5d92a12ceaea4127
lovelove33j1 <lovelove33j1@naver.com>
2021-11-15 13:08:34 +0900

    bye로직 추가

--- Example/YeomPrint/ViewController.swift
+++ Example/YeomPrint/ViewController.swift
@@ -15,6 +15,7 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         Yeom.sayHello(with: "성훈")
+        Yeom.sayBye(with: "성훈")
     }
 
     override func didReceiveMemoryWarning() {
--- YeomPrint/Classes/ReplaceMe.swift
+++ YeomPrint/Classes/ReplaceMe.swift
@@ -4,4 +4,8 @@ public struct Yeom {
     public static func sayHello(with name:String) {
         print("\(name)님 안녕하세요.")
     }
+    
+    public static func sayBye(with name:String) {
+        print("\(name)님 안녕히가세요.")
+    }
 }
